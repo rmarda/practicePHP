e8529a589b7d06710ac8cccfbf16a60f4d2652e6
Ruchika Marda <rmarda4@gmail.com>
2013-10-02 00:39:58 -0400

    included default.css in the index page

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <title> Practice PHP Page </title>
+        <link href="default.css" rel="stylesheet" type="text/css" media="screen">
     </head>
     <body>
         <h1> Practice PHP</h1>
