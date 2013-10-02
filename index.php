92667ebd565c6067a376de5c06e225b2fa6b029a
Ruchika Marda <rmarda4@gmail.com>
2013-10-02 00:20:10 -0400

    Added a new index.php file

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> Practice PHP Page </title>
+    </head>
+    <body>
+        <h1> Practice PHP</h1>
+        <?php echo 'This is a practice page;'?>
+    </body>
+</html>
