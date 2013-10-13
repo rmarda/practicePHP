8555a2453ed363ccfffb23f5dbc695ba397ba926
Ruchika Marda <rmarda4@gmail.com>
2013-10-13 16:58:44 -0400

    added Logic.php file

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
     <head>
         <meta charset="utf-8" />
         <title> Practice PHP Page </title>
+        <?php require_once('logic.php');?>
         <link href="default.css" rel="stylesheet" type="text/css" media="screen">
     </head>
     <body>
         <h1> Practice PHP</h1>
-        <?php echo 'This is a practice page';?>
+        The square of 4 is: <?=$square?>
     </body>
 </html>
